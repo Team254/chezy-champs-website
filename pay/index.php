ee02ace4862b351b7fcd4f8f925806fe2b5ac434
Nick Eyre <nick@njeyre.com>
2018-06-15 14:19:53 -0700

    Pay redirect v2

--- pay/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-header("Location: https://www.bcp.org/calendars/events.aspx?action=ShowEventRegistrationForm&EventId=866e2e45-75a4-4e85-9aac-20c5d0d58235&CurrentModuleId=576&CurrentParentModuleId=575");
-die();
